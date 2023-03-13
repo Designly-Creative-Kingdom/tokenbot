ef85c86b7c7fa590aabd5af4e26c42cb0b56bb56
alpaz <91884588+alpazified@users.noreply.github.com>
2023-03-13 01:44:03 -0400

    Update example.ts

--- src/schemas/example.ts
+++ src/schemas/example.ts
@@ -1,8 +1,7 @@
 import { Schema, model, SchemaTypes } from 'mongoose';
-import { type PostingSchemaType } from '../typings/models';
 
 const schema = new Schema({
     example: { type: SchemaTypes.String }
 }, { versionKey: false });
 
-export const postModel = model<PostingSchemaType>('posting', schema);
+export const postModel = model('example', schema);
