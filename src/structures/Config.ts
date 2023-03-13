57a05b000eecf7e3fbcc2e422fb2b12f168287ea
alpaz <91884588+alpazified@users.noreply.github.com>
2023-03-13 01:42:50 -0400

    Update Config.ts

--- src/structures/Config.ts
+++ src/structures/Config.ts
@@ -17,14 +17,12 @@ export class Config {
 
 	public async updateAll() {
 		this.general = {
-			developers: [],
+			developers: [''],
 			appealLink: '',
-			jobPostingChannel: '1064269297075105944',
-			priorityPostChannel: '1064267202951393370'
 		}
 		this.logging = {
-			webhook: new WebhookClient({ url: 'https://discord.com/api/webhooks/1059219126284980244/MXr0i2rY5MpQdNmQttabpk2TKVcx6bITJ_b8mpw-fLzUSSkmh4ecaHdfEa67o1jBRCs5' }),
-			reports: { channelId: '1059219108954120213', active: true }
+			webhook: new WebhookClient({ url: '' }),
+			reports: { channelId: '', active: true }
 		};
 	};
 };
