cd62d070bc53f85ff80a77587eddd63d38b647c2
alpaz <91884588+alpazified@users.noreply.github.com>
2023-05-14 19:53:38 -0400

    cleaning up + version update

--- src/schemas/example.ts
+++ src/schemas/example.ts
@@ -1,8 +1,8 @@
 import { Schema, model, SchemaTypes } from 'mongoose';
-import { type PostingSchemaType } from '../typings/models';
+import { type ExampleSchemaType } from '../typings/models';
 
 const schema = new Schema({
     example: { type: SchemaTypes.String }
 }, { versionKey: false });
 
-export const postModel = model<PostingSchemaType>('posting', schema);
+export const exampleModel = model<ExampleSchemaType>('example', schema);
--- src/structures/Config.ts
+++ src/structures/Config.ts
@@ -11,20 +11,18 @@ export class Config {
 	public general = {
 		developers: [] as string[],
 		appealLink: null as string,
-		jobPostingChannel: null as string,
-		priorityPostChannel: null as string,
+
 	};
 
 	public async updateAll() {
 		this.general = {
 			developers: [],
 			appealLink: '',
-			jobPostingChannel: '1064269297075105944',
-			priorityPostChannel: '1064267202951393370'
+
 		}
 		this.logging = {
-			webhook: new WebhookClient({ url: 'https://discord.com/api/webhooks/1059219126284980244/MXr0i2rY5MpQdNmQttabpk2TKVcx6bITJ_b8mpw-fLzUSSkmh4ecaHdfEa67o1jBRCs5' }),
-			reports: { channelId: '1059219108954120213', active: true }
+			webhook: new WebhookClient({ url: '' }),
+			reports: { channelId: '', active: true }
 		};
 	};
 };
